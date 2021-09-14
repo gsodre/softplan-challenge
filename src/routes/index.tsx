a240cd2302c58f64d330116690b38660a9a8b8d3
Gabriel Sodré <gsodre@gvdasa.com.br>
2021-09-14 11:04:44 -0300

    Folder structure and project base.

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import { Home } from "../pages/Home";
+import { Character } from "../pages/Character";
+
+const Routes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/details" component={Character} />
+        <Route path="*" component={() => <Redirect to="/" />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
